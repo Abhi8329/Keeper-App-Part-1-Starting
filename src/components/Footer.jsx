dbb4e84c7f2fd67eeccff033690f76a352a57fd9
Abhishek Sunil Uphade <113494940+Abhi8329@users.noreply.github.com>
2024-08-09 10:22:33 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <footer>
+      copyright {year}
+      {}
+    </footer>
+  );
+}
+export default Footer;
